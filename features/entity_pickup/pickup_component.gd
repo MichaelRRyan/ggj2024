7635bf8e539d2dca4304aa23444a863dfaffdb0c
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2024-01-27 14:09:44 +0000

    Added pickup and let go

--- features/entity_pickup/pickup_component.gd
+++ features/entity_pickup/pickup_component.gd
@@ -2,13 +2,33 @@ extends Area2D
 
 var _is_held = false
 var _mouse_hovered := false
+var _parent : CharacterBody2D = null
 
+
+#-------------------------------------------------------------------------------
+func _ready():
+	var parent = get_parent()
+	if parent is CharacterBody2D:
+		_parent = parent
+	else:
+		queue_free() # Remove this component from the scene, it won't work.
+		print_debug("Pickup component doesn't have a CharacterBody2D parent, removing from scene...")
+
+
+#-------------------------------------------------------------------------------
+func _process(delta):
+	if _is_held:
+		_parent.position = get_global_mouse_position()
+
+#-------------------------------------------------------------------------------
 func _on_mouse_entered():
 	_mouse_hovered = true
 
+#-------------------------------------------------------------------------------
 func _on_mouse_exited():
 	_mouse_hovered = false
 
+#-------------------------------------------------------------------------------
 func _input(event):
 	if event.is_action_pressed("select") and _mouse_hovered:
 		if not Global.mouse.is_holding_entity:
@@ -20,3 +40,5 @@ func _input(event):
 		Global.mouse.is_holding_entity = false
 		_is_held = false
 		print("dropped")
+
+#-------------------------------------------------------------------------------
