df237a223fa6d295e3b8b9078372dfe51787d58b
NicholasEmilGabriel-Casaneanu <116353430+NicholasEmilGabriel-Casaneanu@users.noreply.github.com>
2024-01-27 17:28:26 +0000

    Cleaned code

--- Characters/tree.gd
+++ Characters/tree.gd
@@ -1,16 +1,4 @@
 extends StaticBody2D
 
-var chop_timer
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func chop():
 	print("chopped")
-	pass
