141431acd3095853b49d269b20fede97c53ffb0c
NicholasEmilGabriel-Casaneanu <116353430+NicholasEmilGabriel-Casaneanu@users.noreply.github.com>
2024-01-27 15:09:43 +0000

    minion searches for tree
    minion has an area 2d that checks for tree body, and moves toward it

--- Characters/basic-minion.gd
+++ Characters/basic-minion.gd
@@ -7,6 +7,12 @@ extends CharacterBody2D
 var fall_strength : int = 0;
 @export var fall_dmg_thershold : float = 20.0
 @export var fall_dmg_ratio : float = 1.0
+var has_axe : bool = false
+var is_moving : bool = true
+var has_target : bool = false
+var target_x : float = 0.0
+var target_x_diff : float = 0.0
+var direction : int = 0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -30,12 +36,27 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = jump_velocity
 
+	if has_target:
+		target_x_diff = get_global_position().x - target_x
+		if target_x_diff < 0.0:
+			direction = 1
+		else:
+			direction = -1
+	else:
+		direction = 0
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	#if has_target:
+	velocity.x = direction * speed
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
+
+func _on_view_body_entered(body):
+	if body.is_in_group("tree"):
+		has_target = true
+		target_x = body.get_global_position().x
+		print(target_x)
+	pass # Replace with function body.
