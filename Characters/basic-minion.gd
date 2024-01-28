6d64b3dd7ce986f88e2a48cae820e5b07430b44a
NicholasEmilGabriel-Casaneanu <116353430+NicholasEmilGabriel-Casaneanu@users.noreply.github.com>
2024-01-28 13:47:00 +0000

    Added hammer and building
    * Added resource entity
    
    Inherits entity, part of "resource" group
    
    * Fixed tree chop mechainc
    
    Minions will now search for tree again, if tree disappears, will return to idle
    
    * Added resource drop  on tree chop
    
    tree drops 3 resource
    
    * Made tree targetting depend on a bool
    
    exported has_axe bool will decide if tree is targeted
    
    * Minions harvest all within view
    
    Minions will now harvest entities already within view
    
    * Added build mechanic
    
    hammer minions turn 3 wood to house
    
    * Code rework
    
    * Finished merge
    
    * Removed unused code
    
    ---------
    
    Co-authored-by: Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>

--- Characters/basic-minion.gd
+++ Characters/basic-minion.gd
@@ -10,18 +10,29 @@ signal died(name)
 @export var max_health : float = 10
 @export var _fall_dmg_thershold : float = 200.0
 @export var _fall_dmg_ratio : float = 0.01
+@export var has_axe : bool
+@export var has_hammer : bool
+@export var build_type : PackedScene
+@onready var level_parent = get_parent()
+var target_array = Array()
+var target_array_counter : int = 0
+var interact_array = Array()
+var interact_array_counter : int = 0
 var _prev_velocity := Vector2.ZERO
 var _prev_is_on_floor = false
 var target
 var is_interacting : bool = false
 var has_task : bool = false
-var has_axe : bool = false
 var is_moving : bool = true
 var has_target : bool = false
 var target_x : float = 0.0
 var target_x_diff : float = 0.0
 var rng = RandomNumberGenerator.new()
 var random_number = 0.0
+
+var view_array
+@onready var view_area = $View
+@onready var interact_area = $InteractRange
 
 @onready var harvest_timer = $TimerHarvest
 
@@ -36,16 +47,38 @@ func _ready():
 
 func _physics_process(delta):
 	var direction = 0
-	
 	if is_on_floor():
 		if not _prev_is_on_floor and _prev_velocity.y >= _fall_dmg_thershold:
 			take_damage((_prev_velocity.y - _fall_dmg_thershold) * _fall_dmg_ratio)
 			print(_prev_velocity)
 		velocity.x *= _ground_friction
 		
-		if has_target:
+		if not has_target && not is_interacting && not has_target:
+			get_view_array()
+			for n in view_array.size():
+				if has_axe:
+					if view_array[n].is_in_group("tree"):
+						target = view_array[n]
+						has_target = true
+						has_task = true
+						target_x = view_array[n].get_global_position().x
+				if has_hammer:
+					if view_array[n].is_in_group("resource"):
+						target_array.append(view_array[n])
+						target_array_counter += 1
+						if target_array.size() >= 3:
+							has_target = true
+							has_task = true
+							target_x = view_array[n].get_global_position().x
+							target = view_array[n]
+							
+				
+		
+		if has_target && not is_interacting:
 			target_x_diff = get_global_position().x - target_x
-			if target_x_diff < 0.0:
+			print(target_x_diff)
+			print(is_interacting)
+			if target_x_diff <= 0.0:
 				direction = 1
 			else:
 				direction = -1
@@ -75,15 +108,6 @@ func _physics_process(delta):
 	_prev_is_on_floor = is_on_floor()
 	move_and_slide()
 
-func _on_view_body_entered(body):
-	if not is_interacting:
-		if body.is_in_group("tree"):
-			target = body
-			has_target = true
-			has_task = true
-			target_x = body.get_global_position().x
-			print(target_x)
-
 
 func _on_timer_idle_timeout():
 	if not has_target:
@@ -94,19 +118,68 @@ func _on_timer_idle_timeout():
 
 func _on_interact_range_body_entered(_body):
 	if has_target:
-		has_target = false
-		is_interacting = true
-		has_task = true
-		harvest_timer.start(3)
+		if target.is_in_group("tree"):
+			has_target = false
+			is_interacting = true
+			has_task = true
+			harvest_timer.start(3)
+		if target.is_in_group("resource"):
+			has_target = false
+			is_interacting = true
+			has_task = true
+			harvest_timer.start(3)
+			#building
 
 
 func _on_timer_harvest_timeout():
-	target.chop()
-	target.queue_free()
-	harvest_timer.stop()
+	if target != null && target.is_in_group("tree"):
+		target.chop()
+		target.queue_free()
+		harvest_timer.stop()
+		is_interacting = false
+		has_task = false
+		has_target = false
+		target = null
+		reset_bools()
+	elif target != null && target.is_in_group("resource"):
+		for n in target_array.size():
+			target_array[n].queue_free()
+		harvest_timer.stop()
+		is_interacting = false
+		has_task = false
+		has_target = false
+		target = null
+		build_structure()
+		reset_bools()
+		target_array = []
+	else:
+		harvest_timer.stop()
+		is_interacting = false
+		has_task = false
+		has_target = false
+		reset_bools()
+
+func reset_bools():
 	is_interacting = false
 	has_task = false
-	pass # Replace with function body.
+	has_target = false
+	target = null
+
+func get_view_array():
+	view_array = view_area.get_overlapping_bodies()
+	pass
+	
+func get_interact_array():
+	interact_array = interact_area.get_overlapping_bodies()
+	pass
+
+func build_structure():
+	var structure_instance : Entity = build_type.instantiate() as Entity
+	level_parent.add_child(structure_instance)
+	structure_instance.position = position
+	structure_instance.position.y -= 50
+	#structure_instance.position.x += random_number * 10
+	pass
 
 func take_damage(amount : float):
 	health -= amount
